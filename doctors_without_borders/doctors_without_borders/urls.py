d3ff081ab4c2581b48c520b37ace2fed4ca76041
Kevin Djabaku Ocansey <kevinocansey11@gmail.com>
2025-03-28 21:28:05 +0000

    static styles colelcted updates to main urls

--- doctors_without_borders/doctors_without_borders/urls.py
+++ doctors_without_borders/doctors_without_borders/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include ('users.urls')),
     path("appointments/", include("dwb_calendar.urls")),
+
+    path('', include ('users.urls')),
+    
     path('', include ('appointments.urls')),
     path('', include ('notifications.urls')),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
